705a26c8df5d6943d6218c098b4770b115583bf0
Christian Mulato <chrvlamulato.develcode@rd.com.br>
2023-12-07 14:25:24 -0300

    Exercício sobre layout.

---
+++ src/main/java/com/example/application/solutions/LayoutView.java
@@ -0,0 +1,42 @@
+package com.example.application.solutions;
+
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+
+@Route(value= "/exercises02")
+public class LayoutView extends VerticalLayout {
+
+    private static VerticalLayout getVerticalLayout() {
+        DatePicker datePicker01 = new DatePicker("DatePicker");
+        TextField textField01 = new TextField("TextField");
+        ComboBox<String> comboBox01 = new ComboBox<>("ComboBox");
+        VerticalLayout verticalLayout = new VerticalLayout();
+        verticalLayout.setPadding(true);
+
+        verticalLayout.add(datePicker01);
+        verticalLayout.add(textField01);
+        verticalLayout.add(comboBox01);
+        return verticalLayout;
+    }
+    private static HorizontalLayout getHorizontalLayout() {
+        DatePicker datePicker02 = new DatePicker("DatePicker");
+        TextField textField02 = new TextField("TextField");
+        ComboBox<String> comboBox02 = new ComboBox<>("ComboBox");
+        HorizontalLayout horizontalLayout = new HorizontalLayout();
+        horizontalLayout.setPadding(true);
+        horizontalLayout.add(datePicker02);
+        horizontalLayout.add(textField02);
+        horizontalLayout.add(comboBox02);
+        return horizontalLayout;
+    }
+    LayoutView() {
+        VerticalLayout verticalLayout = getVerticalLayout();
+        HorizontalLayout horizontalLayout = getHorizontalLayout();
+        HorizontalLayout mainLayout = new HorizontalLayout(verticalLayout, horizontalLayout);
+        add(mainLayout);
+    }
+}
